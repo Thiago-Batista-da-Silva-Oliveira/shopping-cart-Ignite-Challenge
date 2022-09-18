0d88dbc60b710c07258489290d5eb81d1a7d9d8c
Thiago <gmoliveira69@gmail.com>
2022-09-18 17:44:51 -0300

    main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { AppProvider } from './providers';
-import { AppRoutes } from './routes';
 
-function App() {
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
+
+const App = (): JSX.Element => {
   return (
-    <>
-    <AppProvider>
-      <AppRoutes />
-    </AppProvider>
-    </>
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
